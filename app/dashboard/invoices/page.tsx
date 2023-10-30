93ad2b0030fcbf2b3e2bb1611712edac30efd3a7
= <enriqueleonq1@gmail.com>
2023-10-30 11:29:50 -0400

    Initial commit for setup db

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+
+export default function InvoicesPages () {
+    return (
+        <p>Invoices pages</p>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage () {
+    return(
+        <p>Este es el componente de dashboard</p>
+    )
+}
